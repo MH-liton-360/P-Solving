0ed643237b8624e2597485f76214263aaf4e7338
MH-liton-360 <liton72524nk@gmail.com>
2025-07-02 00:44:21 +0600

    first commit

---
+++ CodeForces/G_Factorial.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        /* code */
+        int n;
+        cin >> n;
+        long long fact = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            /* code */
+            fact *= i;
+        }
+        cout << fact << endl;
+    }
+
+    return 0;
+}
