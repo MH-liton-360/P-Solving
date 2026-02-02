fa5f9816951e4c520d3404acee41f63266b16724
MH-liton-360 <liton72524nk@gmail.com>
2026-02-02 22:59:31 +0600

    resolve

--- CodeForces/A_Beautiful_Year.cpp
+++ CodeForces/A_Beautiful_Year.cpp
@@ -5,13 +5,11 @@ int main()
 {
     int y;
     cin >> y;
-
     while (true)
     {
         y++;
         string s = to_string(y);
 
-        // set ব্যবহার করে duplicate check
         set<char> digits(s.begin(), s.end());
 
         if (digits.size() == s.size())
