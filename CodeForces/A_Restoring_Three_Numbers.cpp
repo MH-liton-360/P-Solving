4479fc6e74a80228c8cd1f4821538b559e44c5c5
MH-liton-360 <liton72524nk@gmail.com>
2026-02-12 18:18:24 +0600

    Restoring three numbers

---
+++ CodeForces/A_Restoring_Three_Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long x[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> x[i];
+    }
+
+    sort(x, x + 4);
+
+    long long total = x[3]; // a+b+c
+
+    cout << total - x[2] << " "
+         << total - x[1] << " "
+         << total - x[0] << endl;
+
+    return 0;
+}
